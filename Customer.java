a5a5ad85579f35eba04a1a8286b9d6cd0099a79c
Brian Ho <brianho.net@gmail.com>
2024-10-27 03:19:13 -0400

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,19 @@
+package com.queuesimulation;
+
+public class Customer {
+    private final int id;
+    private final int serviceTime;
+
+    public Customer(int id, int serviceTime) {
+        this.id = id;
+        this.serviceTime = serviceTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+}
